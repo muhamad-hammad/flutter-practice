12360401e5803da3f8775d6c4686a7847546d2fa
muhamad-hammad <m.hammad.bhatti09@gmail.com>
2024-08-05 19:47:16 +0500

    updated themes colors

--- flutter_ecommerceapp_wcart/lib/theme/theme.dart
+++ flutter_ecommerceapp_wcart/lib/theme/theme.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
-  brightness: Brightness.light,
-  colorScheme: ColorScheme.light(
-    surface: Colors.grey.shade900,
-    primary:  Colors.grey.shade800,
-    secondary:  Colors.grey.shade700,
-    inversePrimary:  Colors.grey.shade600,
-  )
-);
-
-ThemeData darkMode = ThemeData(
-  brightness: Brightness.dark,
-  colorScheme: ColorScheme.dark (
-    surface: Colors.grey.shade900,
-    primary:  Colors.grey.shade800,
-    secondary:  Colors.grey.shade700,
-    inversePrimary:  Colors.grey.shade600,
-  )
-);
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      surface: Colors.grey.shade600,
+      primary: Colors.grey.shade500,
+      secondary: Colors.white,
+      inversePrimary: Colors.grey.shade800,
+    ));
